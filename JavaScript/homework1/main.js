2cd552008bc427a99088c45a896ec7280eab10f3
Blackberry82 <morozir@gmail.com>
2022-04-14 15:07:28 +0300

    add js hw1

---
+++ JavaScript/homework1/main.js
@@ -0,0 +1,139 @@
+let hello = 'hello';
+console.log(hello);
+alert('hello');
+document.write('hello');
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua);
+
+let a = 1;
+console.log(a);
+
+let b = 10;
+console.log(b);
+
+let c = -999;
+console.log(c);
+
+const num = 3.14;
+console.log(num);
+
+let d = 1>3;
+console.log(d);
+
+let e = (20 + 2) === 22;
+console.log(e);
+
+let book = {
+    name: 'Spy',
+    pages: 125,
+    gerne: 'detective'
+    }
+    console.log(book);
+
+let book1 = {
+    name: 'Теплі історії в конвертах',
+    pages: 150,
+    gerne: 'biography',
+    autors: [
+        {name: 'Олександра Орлова'},
+        {name: 'Ірина Мацко'}
+    ]
+    }
+console.log(book1);
+
+let firstName = 'Ірина';
+let middleName = 'Ігорівна';
+let lastName = 'Мирська'
+
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+
+let n = 'Iryna';
+let f = 'Igorivna';
+let y = 40;
+
+console.log(`${n} ${f} ${y}`);
+console.log(`Iryna Igorivna ${y}`)
+
+let a1 = 100;
+console.log(typeof a1);
+let b1 = '100';
+console.log(typeof b1);
+let c1 = true;
+console.log(typeof c1);
+
+let books = [
+    {name: 'Joyland', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King', yesr: 2013},
+    {name: 'Dr. Sleep', pages: 138, gerne: 'roman', autor: 'Stephen Edwin King', year: 2013},
+    {name: 'Mr. Mercedes', pages: 146, gerne: 'roman', autor: 'Stephen Edwin King', yesr: 2014},
+    {name: 'Revival', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King', yesr: 2014},
+    {name: 'Finders Keepers', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King', year: 2015},
+    {name: 'The Bazaar of Bad Dreams', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King', yesr: 2015},
+    {name: 'End of watch', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King',yesr: 2016},
+    {name: 'Outsider', pages: 123, gerne: 'roman', autor: 'Stephen Edwin King', yesr: 2018},
+]
+
+console.log(books[0]);
+console.log(books[0].yesr);
+console.log(books[1]);
+console.log(books[1].autor);
+console.log(books[2]);
+console.log(books[2].name);
+console.log(books[3]);
+console.log(books[3].pages);
+console.log(books[4]);
+console.log(books[4].gerne);
+console.log(books[5]);
+console.log(books[6]);
+console.log(books[7]);
+
+let q = (5 < 6);
+console.log(q);
+
+let w = 5 > 6;
+console.log(w);
+
+let r = 5 >= 6;
+console.log(r);
+
+let t = 5 === 6;
+console.log(t);
+
+let u = 10 <= 10;
+console.log(u);
+
+let i = 10 === 10;
+console.log(i);
+
+let o = 10 < 10;
+console.log(o);
+
+let p = 10 > 10;
+console.log(p);
+
+let s = 10 !== 10;
+console.log(s);
+
+let h = 123 === '123';
+console.log(h);
+
+let k = 123 >= '123';
+console.log(k);
+
+
+
+
+
+
+
+
+
+
+
+
