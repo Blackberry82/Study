c2749ef1c93c5537dcde36995201eadef029fb61
Blackberry82 <morozir@gmail.com>
2022-05-17 06:14:23 +0300

    add js hw9

---
+++ JavaScript/homework9/main.js
@@ -0,0 +1,357 @@
+
+// - створити блок,
+// const  blocks = document.createElement('div');
+// blocks.innerText = 'Hello, my Heart!';
+
+//     - додати йому класи wrap, collapse, alpha, beta
+// blocks.classList.add('wrap', 'collapse', 'alpha', 'beta');
+
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// blocks.style.background = 'green';
+// blocks.style.color = 'white';
+// blocks.style.fontSize = '100px';
+
+// - додати цей блок в body.
+// const blockDiv = document.body.appendChild(blocks);
+
+// - клонувати його повністю, та додати клон в body.
+// const Clone = blocks.cloneNode(true);
+// document.body.appendChild(Clone);
+
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+// let info = ['Main','Products','About us','Contacts'];
+// for (let i = 0; i < info.length; i++) {
+//     const infoElement = info[i];
+//     const section = document.createElement('li');
+//     section.innerText = info[i];
+//     const lists = document.getElementsByTagName('ul');
+//     for (const list of lists) {
+//         list.appendChild(section);
+//     }
+// }
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// // Завдання робити через цикли.
+// for (const item of coursesAndDurationArray) {
+//     const oneDiv = document.createElement('div');
+//     oneDiv.innerHTML = `
+//     <h3>${item.title}</h3>
+//     <p>${item.monthDuration}</p><hr>
+//     `
+//     document.body.appendChild(oneDiv);
+// }
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
+// for (const Element of coursesAndDurationArray) {
+//     const one = document.createElement('div');
+//     document.body.appendChild(one);
+//     one.classList.add('item');
+//     const header = document.createElement('h1');
+//     header.classList.add('heading');
+//     header.innerText = `${Element.title}`;
+//     one.appendChild(header);
+//     const paragraf = document.createElement('p');
+//     paragraf.classList.add('description');
+//     paragraf.innerText = `${Element.monthDuration}`;
+//     one.appendChild(paragraf);
+// // }
+
+let usersList = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+                lat: '-37.3159',
+                lng: '81.1496'
+            }
+        },
+        phone: '1-770-736-8031 x56442',
+        website: 'hildegard.org',
+        company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets'
+        }
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        address: {
+            street: 'Victor Plains',
+            suite: 'Suite 879',
+            city: 'Wisokyburgh',
+            zipcode: '90566-7771',
+            geo: {
+                lat: '-43.9509',
+                lng: '-34.4618'
+            }
+        },
+        phone: '010-692-6593 x09125',
+        website: 'anastasia.net',
+        company: {
+            name: 'Deckow-Crist',
+            catchPhrase: 'Proactive didactic contingency',
+            bs: 'synergize scalable supply-chains'
+        }
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        address: {
+            street: 'Douglas Extension',
+            suite: 'Suite 847',
+            city: 'McKenziehaven',
+            zipcode: '59590-4157',
+            geo: {
+                lat: '-68.6102',
+                lng: '-47.0653'
+            }
+        },
+        phone: '1-463-123-4447',
+        website: 'ramiro.info',
+        company: {
+            name: 'Romaguera-Jacobson',
+            catchPhrase: 'Face to face bifurcated interface',
+            bs: 'e-enable strategic applications'
+        }
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        address: {
+            street: 'Hoeger Mall',
+            suite: 'Apt. 692',
+            city: 'South Elvis',
+            zipcode: '53919-4257',
+            geo: {
+                lat: '29.4572',
+                lng: '-164.2990'
+            }
+        },
+        phone: '493-170-9623 x156',
+        website: 'kale.biz',
+        company: {
+            name: 'Robel-Corkery',
+            catchPhrase: 'Multi-tiered zero tolerance productivity',
+            bs: 'transition cutting-edge web services'
+        }
+    },
+    {
+        id: 5,
+        name: 'Chelsey Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        address: {
+            street: 'Skiles Walks',
+            suite: 'Suite 351',
+            city: 'Roscoeview',
+            zipcode: '33263',
+            geo: {
+                lat: '-31.8129',
+                lng: '62.5342'
+            }
+        },
+        phone: '(254)954-1289',
+        website: 'demarco.info',
+        company: {
+            name: 'Keebler LLC',
+            catchPhrase: 'User-centric fault-tolerant solution',
+            bs: 'revolutionize end-to-end systems'
+        }
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        address: {
+            street: 'Norberto Crossing',
+            suite: 'Apt. 950',
+            city: 'South Christy',
+            zipcode: '23505-1337',
+            geo: {
+                lat: '-71.4197',
+                lng: '71.7478'
+            }
+        },
+        phone: '1-477-935-8478 x6430',
+        website: 'ola.org',
+        company: {
+            name: 'Considine-Lockman',
+            catchPhrase: 'Synchronised bottom-line interface',
+            bs: 'e-enable innovative applications'
+        }
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        address: {
+            street: 'Rex Trail',
+            suite: 'Suite 280',
+            city: 'Howemouth',
+            zipcode: '58804-1099',
+            geo: {
+                lat: '24.8918',
+                lng: '21.8984'
+            }
+        },
+        phone: '210.067.6132',
+        website: 'elvis.io',
+        company: {
+            name: 'Johns Group',
+            catchPhrase: 'Configurable multimedia task-force',
+            bs: 'generate enterprise e-tailers'
+        }
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        address: {
+            street: 'Ellsworth Summit',
+            suite: 'Suite 729',
+            city: 'Aliyaview',
+            zipcode: '45169',
+            geo: {
+                lat: '-14.3990',
+                lng: '-120.7677'
+            }
+        },
+        phone: '586.493.6943 x140',
+        website: 'jacynthe.com',
+        company: {
+            name: 'Abernathy Group',
+            catchPhrase: 'Implemented secondary concept',
+            bs: 'e-enable extensible e-tailers'
+        }
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        address: {
+            street: 'Dayna Park',
+            suite: 'Suite 449',
+            city: 'Bartholomebury',
+            zipcode: '76495-3109',
+            geo: {
+                lat: '24.6463',
+                lng: '-168.8889'
+            }
+        },
+        phone: '(775)976-6794 x41206',
+        website: 'conrad.com',
+        company: {
+            name: 'Yost and Sons',
+            catchPhrase: 'Switchable contextually-based project',
+            bs: 'aggregate real-time technologies'
+        }
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        address: {
+            street: 'Kattie Turnpike',
+            suite: 'Suite 198',
+            city: 'Lebsackbury',
+            zipcode: '31428-2261',
+            geo: {
+                lat: '-38.2386',
+                lng: '57.2232'
+            }
+        },
+        phone: '024-648-3804',
+        website: 'ambrose.net',
+        company: {
+            name: 'Hoeger LLC',
+            catchPhrase: 'Centralized empowering task-force',
+            bs: 'target end-to-end models'
+        }
+    }
+];
+
+// Створити під кожен елемент окремий блок. В цьому блоці, під кожну властивість, та властивості внутрішніх об'єктів створити свої окремі блок.
+const group = document.createElement('div');
+for (const user of usersList) {
+    const user1 = document.createElement('div')
+
+    const id = document.createElement('h2');
+    id.innerText = `${user.id}`;
+
+    const name = document.createElement('h2');
+    name.innerText = `${user.name}`;
+
+    const username = document.createElement('h3');
+    username.innerText = `${user.username}`;
+
+    const email = document.createElement('div');
+    email.innerText = `${user.email}`;
+
+    const address = document.createElement('div');
+    address.innerHTML = `${user.address.street} <br> ${user.address.suite} <br> ${user.address.city} <br> ${user.address.zipcode}`
+    const geo = document.createElement('p');
+    geo.innerHTML = `${user.address.geo.lat} <br> ${user.address.geo.lng}`;
+
+    const phone = document.createElement('p');
+    phone.innerText = `${user.phone}`;
+
+    const web = document.createElement('p');
+    web.innerText = `${user.website}`;
+
+    const company = document.createElement('div');
+    company.innerHTML = `${user.company.name} <br> ${user.company.catchPhrase} <br> ${user.company.bs}`;
+group.classList.add('person');
+    user1.appendChild(id);
+    user1.appendChild(name);
+    user1.appendChild(username);
+    user1.appendChild(email);
+    user1.appendChild(address);
+    address.appendChild(geo);
+    user1.appendChild(phone);
+    user1.appendChild(web);
+    user1.appendChild(company);
+    group.appendChild(user1);
+    document.body.appendChild(group);
+
+}
+
