21c2dab9b9ce3dec436e1acdb03eeec95abb2cfa
Blackberry82 <morozir@gmail.com>
2022-05-18 03:29:55 +0300

    add js hw9 add

--- JavaScript/homework9/main.js
+++ JavaScript/homework9/main.js
@@ -311,47 +311,210 @@ let usersList = [
 ];
 
 // Створити під кожен елемент окремий блок. В цьому блоці, під кожну властивість, та властивості внутрішніх об'єктів створити свої окремі блок.
-const group = document.createElement('div');
-for (const user of usersList) {
-    const user1 = document.createElement('div')
+// const group = document.createElement('div');
+// for (const user of usersList) {
+//     const user1 = document.createElement('div')
+//
+//     const id = document.createElement('h2');
+//     id.innerText = `${user.id}`;
+//
+//     const name = document.createElement('h2');
+//     name.innerText = `${user.name}`;
+//
+//     const username = document.createElement('h3');
+//     username.innerText = `${user.username}`;
+//
+//     const email = document.createElement('div');
+//     email.innerText = `${user.email}`;
+//
+//     const address = document.createElement('div');
+//     address.innerHTML = `${user.address.street} <br> ${user.address.suite} <br> ${user.address.city} <br> ${user.address.zipcode}`
+//     const geo = document.createElement('p');
+//     geo.innerHTML = `${user.address.geo.lat} <br> ${user.address.geo.lng}`;
+//
+//     const phone = document.createElement('p');
+//     phone.innerText = `${user.phone}`;
+//
+//     const web = document.createElement('p');
+//     web.innerText = `${user.website}`;
+//
+//     const company = document.createElement('div');
+//     company.innerHTML = `${user.company.name} <br> ${user.company.catchPhrase} <br> ${user.company.bs}`;
+// group.classList.add('person');
+//     user1.appendChild(id);
+//     user1.appendChild(name);
+//     user1.appendChild(username);
+//     user1.appendChild(email);
+//     user1.appendChild(address);
+//     address.appendChild(geo);
+//     user1.appendChild(phone);
+//     user1.appendChild(web);
+//     user1.appendChild(company);
+//     group.appendChild(user1);
+//     document.body.appendChild(group);
+//
+// }
 
-    const id = document.createElement('h2');
-    id.innerText = `${user.id}`;
+// - є масив
+let simpsons = [
+    {
+        name: 'Bart',
+        surname: 'Simpson',
+        age: 10,
+        info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+        photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+    },
+    {
+        name: 'Homer',
+        surname: 'Simpson',
+        age: 40,
+        info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+        photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+    },
+    {
+        name: 'Marge',
+        surname: 'Simpson',
+        age: 38,
+        info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+    },
+    {
+        name: 'Lisa',
+        surname: 'Simpson',
+        age: 9,
+        info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+    },
+    {
+        name: 'Maggie',
+        surname: 'Simpson',
+        age: 1,
+        info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+        photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+    },
+];
+// Проітерувати його, створиши для кожного елементу масиву <div class='member'>.
+// for (const simpson of simpsons) {
+//     const divEl = document.createElement('div');
+//     divEl.setAttribute('class', 'member');
+//
+//     const name = document.createElement('h2');
+//     name.innerText = `${simpson.name} ${simpson.surname}`;
+//
+//     const age = document.createElement('p');
+//     age.innerText = `${simpson.age}`;
+//
+//     const info = document.createElement('p');
+//     info.innerText = `${simpson.info}`;
+//
+//     const photo = document.createElement('img');
+//     // photo.setAttribute('src', `${simpson.photo}`);
+//     photo.src = simpson.photo;
+//     divEl.append(name, age, info, photo);
+//    document.body.appendChild(divEl);
+// }
 
-    const name = document.createElement('h2');
-    name.innerText = `${user.name}`;
+// - взяти попередній масив з сімпсонами.
+//     Проітерувати його, створиши для кожного елементу масиву <div class='member'>. Для кожної властивості елементу створити окремий блок, та помістити його у div.member
+//
+// - Є масив
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+// Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
 
-    const username = document.createElement('h3');
-    username.innerText = `${user.username}`;
+// for (const item of coursesArray) {
+//     const common = document.createElement('div');
+//
+//     const title = document.createElement('h2');
+//     title.innerText = item.title;
+//     const red = Math.random() * 255;
+//     const green = Math.random() * 255;
+//     const blue = Math.random() * 255;
+//     title.style.color = `rgb(${red} ${green} ${blue})`;
+//
+//     const term = document.createElement('p');
+//     term.innerText = item.monthDuration;
+//
+//     const time = document.createElement('p');
+//     time.innerText = item.hourDuration;
+//
+//     const block = document.createElement('ol');
+//     block.style.border = (`solid 5px pink`);
+//     for (const modul of item.modules) {
+//         const list = document.createElement('li');
+//             list.innerHTML = modul;
+//             block.appendChild(list);
+//
+//     }
+//     common.append(title, term, time, block);
+//     document.body.appendChild(common);
+// }
 
-    const email = document.createElement('div');
-    email.innerText = `${user.email}`;
-
-    const address = document.createElement('div');
-    address.innerHTML = `${user.address.street} <br> ${user.address.suite} <br> ${user.address.city} <br> ${user.address.zipcode}`
-    const geo = document.createElement('p');
-    geo.innerHTML = `${user.address.geo.lat} <br> ${user.address.geo.lng}`;
-
-    const phone = document.createElement('p');
-    phone.innerText = `${user.phone}`;
-
-    const web = document.createElement('p');
-    web.innerText = `${user.website}`;
-
-    const company = document.createElement('div');
-    company.innerHTML = `${user.company.name} <br> ${user.company.catchPhrase} <br> ${user.company.bs}`;
-group.classList.add('person');
-    user1.appendChild(id);
-    user1.appendChild(name);
-    user1.appendChild(username);
-    user1.appendChild(email);
-    user1.appendChild(address);
-    address.appendChild(geo);
-    user1.appendChild(phone);
-    user1.appendChild(web);
-    user1.appendChild(company);
-    group.appendChild(user1);
-    document.body.appendChild(group);
-
-}
 
